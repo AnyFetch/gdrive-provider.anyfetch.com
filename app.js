44de4586623780df738318a1998f61ec2fdf15c6
Robin Ricard <ricard.robin@gmail.com>
2013-10-17 20:12:11 +0200

    App

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Load configuration and initialize server
+var cluestrProvider = require('cluestr-provider');
+
+var providerGoogleDrive = require('./lib/provider-google-drive');
+
+var config = require('./config/configuration.js');
+
+var serverHandlers = providerGoogleDrive(config);
+
+var server = cluestrProvider.createServer(serverHandlers);
+
+// Expose the server
+module.exports = server;
