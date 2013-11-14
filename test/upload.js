8f605dede14a77b68bd3d4dddf7dc9f5d9df3b27
Robin Ricard <ricard.robin@gmail.com>
2013-11-14 18:56:23 +0100

    Yay ! tests

--- test/upload.js
+++ test/upload.js
@@ -1,57 +1,52 @@
-// 'use strict';
+'use strict';
 
-// var request = require('supertest');
-// var CluestrProvider = require('cluestr-provider');
-// require('should');
+var request = require('supertest');
+var CluestrProvider = require('cluestr-provider');
+require('should');
 
-// var config = require('../config/configuration.js');
-// var serverConfig = require('../lib/provider-google-contact');
+var config = require('../config/configuration.js');
+var serverConfig = require('../lib/provider-google-drive')(config);
 
+describe("File Upload", function () {
+  // Create a fake HTTP server
+  process.env.CLUESTR_SERVER = 'http://localhost:1337';
 
+  // Create a fake HTTP server
+  var frontServer = CluestrProvider.debug.createTestApiServer();
+  frontServer.listen(1337);
 
-// describe("Workflow", function () {
-//   before(CluestrProvider.debug.cleanTokens);
+  before(CluestrProvider.debug.cleanTokens);
+  before(function(done) {
+    CluestrProvider.debug.createToken({
+      cluestrToken: 'fake_gdrive_access_token',
+      datas: config.test_refresh_token,
+      cursor: process.test_cursor
+    }, done);
+  });
 
-// // Create a fake HTTP server
-//   process.env.CLUESTR_SERVER = 'http://localhost:1337';
+  it("should upload datas to Cluestr", function (done) {
+    var originalQueueWorker = serverConfig.queueWorker;
+    serverConfig.queueWorker = function(task, cluestrClient, dropboxTokens, cb) {
+      task.should.have.property('id');
 
-//   // Create a fake HTTP server
-//   var frontServer = CluestrProvider.debug.createTestApiServer();
-//   frontServer.listen(1337);
+      originalQueueWorker(task, cluestrClient, dropboxTokens, cb);
+    };
+    var server = CluestrProvider.createServer(serverConfig);
 
-//   before(function(done) {
-//     CluestrProvider.debug.createToken({
-//       cluestrToken: 'fake_gc_access_token',
-//       datas: config.test_refresh_token,
-//       cursor: new Date(1970)
-//     }, done);
-//   });
+    server.queue.drain = function() {
+      done();
+    };
 
-//   it("should upload datas to Cluestr", function (done) {
-//     var originalQueueWorker = serverConfig.queueWorker;
-//     serverConfig.queueWorker = function(task, cluestrClient, refreshToken, cb) {
-//       console.log(task.url);
-//       task.should.have.property('url');
-//       task.should.have.property('id');
-
-//       originalQueueWorker(task, cluestrClient, refreshToken, cb);
-//     };
-//     var server = CluestrProvider.createServer(serverConfig);
-
-//     server.queue.drain = function() {
-//       done();
-//     };
-
-//     request(server)
-//       .post('/update')
-//       .send({
-//         access_token: 'fake_gc_access_token'
-//       })
-//       .expect(202)
-//       .end(function(err) {
-//         if(err) {
-//           throw err;
-//         }
-//       });
-//   });
-// });
+    request(server)
+      .post('/update')
+      .send({
+        access_token: 'fake_gdrive_access_token'
+      })
+      .expect(202)
+      .end(function(err) {
+        if(err) {
+          throw err;
+        }
+      });
+  });
+});
