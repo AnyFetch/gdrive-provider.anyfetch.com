71c269ddae971be15902f516ee7fc12eabf8a408
Robin Ricard <ricard.robin@gmail.com>
2013-11-14 21:45:40 +0100

    You know, misconfiguration and stuff ...

--- config_heroku.sh
+++ config_heroku.sh
@@ -10,8 +10,8 @@ URL="http://anyfetch-provider-google-drive.herokuapp.com"
 
 heroku config:set GOOGLE_DRIVE_ID="$GOOGLE_DRIVE_ID"
 heroku config:set GOOGLE_DRIVE_SECRET="$GOOGLE_DRIVE_SECRET"
-heroku config:set GOOGLE_DRIVE_CALLBACK_URL="$URL/init/connect"
-heroku config:set GOOGLE_DRIVE_CONNECT_URL="$URL/init/callback"
+heroku config:set GOOGLE_DRIVE_CALLBACK_URL="$URL/init/callback"
+heroku config:set GOOGLE_DRIVE_CONNECT_URL="$URL/init/connect"
 
 heroku config:set GOOGLE_DRIVE_CLUESTR_ID="$GOOGLE_DRIVE_CLUESTR_ID"
 heroku config:set GOOGLE_DRIVE_CLUESTR_SECRET="$GOOGLE_DRIVE_CLUESTR_SECRET"
