443d60ba495642a72c2eb7143e09d7a63a5c3865
Neamar <neamar@neamar.fr>
2014-05-09 16:19:37 +0200

    Refactoring pages retrieval

--- test/retrieve.js
+++ test/retrieve.js
@@ -19,6 +19,7 @@ describe("Retrieve files", function () {
       done();
     });
   });
+
   it("should list files from a given id", function(done) {
     retrieve(config.test_refresh_token, "44", config, function(err, files, lastId) {
       if(err) {
