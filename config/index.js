9d26b8684287de6b4412e3f9ace9c3637c326be2
Robin Ricard <ricard.robin@gmail.com>
2014-06-16 22:55:18 +0200

    Some config

---
+++ config/index.js
@@ -0,0 +1,34 @@
+use_strict();
+
+module.exports = function(config) {
+  if(!config) {
+    config = {};
+  }
+
+  // Basic configuration
+  config.env  = process.env.NODE_ENV || "development";
+  config.port = process.env.PORT     || 3000;
+
+  // Redis for job queues
+  config.redis = {};
+  config.redis.port = process.env.REDIS_PORT || 6379;
+  config.redis.host = process.env.REDIS_HOST || '127.0.0.1';
+  config.redis.auth = process.env.REDIS_AUTH || null;
+
+  // GDrive OAuth informations
+  config.gdrive = {};
+  config.gdrive.apiId = process.env.GDRIVE_API_ID;
+  config.gdrive.apiSecret = process.env.GDRIVE_API_SECRET;
+  config.gdrive.redirectUri = process.env.GDRIVE_REDIRECT_URI;
+
+  // AnyFetch OAuth informations
+  config.anyfetch = {};
+  config.anyfetch.apiId = process.env.ANYFETCH_API_ID;
+  config.anyfetch.apiSecret = process.env.ANYFETCH_API_SECRET;
+  config.anyfetch.redirectUri = process.env.ANYFETCH_REDIRECT_URI;
+
+  // Environment-bound configuration
+  // config = require('./' + config.env + '.js')(config);
+
+  return config;
+};
