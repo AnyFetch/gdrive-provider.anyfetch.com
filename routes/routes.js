6750c72e9334366391d99335998a89d09385214f
Robin Ricard <ricard.robin@gmail.com>
2014-06-17 10:00:15 +0200

    deprecation warining

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,7 @@ var autoload = require('auto-load');
 var routes = autoload(__dirname);
 
 module.exports = function(app) {
-  app.del('/reset', routes.reset.del);
+  app.delete('/reset', routes.reset.del);
   app.get('/init/connect', routes.init.connect.get);
   app.get('/init/callback', routes.init.callback.get);
 
