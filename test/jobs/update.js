4e23f51e7f68df6574235cc2decb46ca9280500f
Robin Ricard <ricard.robin@gmail.com>
2014-06-17 16:26:48 +0200

    try to test

--- test/jobs/update.js
+++ test/jobs/update.js
@@ -1,3 +1,27 @@
 "use strict";
 
 require('../mock/index.js');
+var update = require('../../jobs/update.js');
+var clean = require('../helpers/clean.js');
+var app = require('../../app.js');
+
+describe("JOB update", function() {
+  before(function bindStore() {
+    this.store = app.get('keyValueStore');
+  });
+  beforeEach(clean);
+
+  it('should succeed and fetch the change id', function(done) {
+    var job = {
+      data: {
+        providerToken: 'aGoogleRefreshToken',
+        cursor: null,
+        anyfetchToken: 'anAccessToken'
+      }
+    };
+    update(app)(job, function assertJobResult(changeId) {
+      changeId.should.be.exactly('change0');
+      done();
+    });
+  });
+});
