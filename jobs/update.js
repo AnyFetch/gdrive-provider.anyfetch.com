b1599102e6bb361e78a49de8ef46966dbd9923d0
Robin Ricard <ricard.robin@gmail.com>
2014-06-17 17:34:26 +0200

    get rid of the sitty this

--- jobs/update.js
+++ jobs/update.js
@@ -2,6 +2,7 @@
 
 var async = require('async');
 var googleapis = require('googleapis');
+var rarity = require('rarity');
 
 var PREFIX = "http://gdrive.provider.anyfetch.com";
 
@@ -9,32 +10,27 @@ module.exports = function(app) {
   var queue = app.get('queue');
 
   return function(job, done) {
-    var context = {};
     async.waterfall([
       function discoverApi(cb) {
         googleapis.discover('drive', 'v2').execute(cb);
       },
       function setup(client, cb) {
-        this.client = client;
-        this.authClient = app.get('googleOAuth');
-        this.authClient.credentials = {
+        var authClient = app.get('googleOAuth');
+        authClient.credentials = {
           access_token: "to_renew",
           refresh_token: job.data.providerToken
         };
-        this.options = {
+        var options = {
           maxResults: 1000,
           startChangeId: job.data.cursor,
           includeDeleted: (!! job.data.cursor)
         };
-        cb(null, [], false);
-      }.bind(context),
-      function retrievePageOfChanges(changes, nextPage, cb) {
-        if(nextPage) {
-          this.options.pageToken = nextPage;
-        }
-        this.client.drive.changes
-          .list(this.options)
-          .withAuthClient(this.authClient)
+        cb(null, [], options, client, authClient);
+      },
+      function retrievePageOfChanges(changes, options, client, authClient, cb) {
+        client.drive.changes
+          .list(options)
+          .withAuthClient(authClient)
           .execute(function mergePageOfChanges(err, res) {
             if(err || !res) {
               return cb(err, res);
@@ -42,20 +38,21 @@ module.exports = function(app) {
 
             changes = changes.concat(res.items);
             if(res.nextPageToken) {
-              retrievePageOfChanges.apply(context, [changes, res.nextPageToken, cb]);
+              options.pageToken = res.nextPageToken;
+              retrievePageOfChanges(changes, options, client, authClient, cb);
             } else {
               cb(null, changes);
             }
-          }.bind(context));
-      }.bind(context),
+          });
+      },
       function squashFiles(changes, cb) {
-        this.lastChangeId = changes[changes.length -1].id;
+        var lastChangeId = changes[changes.length -1].id;
         async.reduce(changes, {}, function reduceFile(files, change, cb) {
           files[PREFIX + change.file.id] = change.deleted?{deleted: true}:change.file;
           cb(null, files);
-        }, cb);
-      }.bind(context),
-      function spawnUploadJobs(files, cb) {
+        }, rarity.carry([lastChangeId], cb));
+      },
+      function spawnUploadJobs(lastChangeId, files, cb) {
         for(var id in files) {
           var file = files[id];
           if(file.deleted) {
@@ -72,8 +69,8 @@ module.exports = function(app) {
             }).priority('high').attempts(5);
           }
         }
-        cb(null, this.lastChangeId);
-      }.bind(context)
+        cb(null, lastChangeId);
+      }
     ], done);
   };
 };
