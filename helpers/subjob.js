9b6331d317ce6cc9f7ebec139d81d99bfe11d33b
Robin Ricard <ricard.robin@gmail.com>
2014-06-18 21:34:49 +0200

    dot save

--- helpers/subjob.js
+++ helpers/subjob.js
@@ -1,9 +1,9 @@
 "use strict";
 
 module.exports.create = function(queue, type, data) {
-  console.log('spawn', type, data);
   return queue
           .create(type, data)
           .priority('high')
-          .attempts(5);
+          .attempts(5)
+          .save();
 };
