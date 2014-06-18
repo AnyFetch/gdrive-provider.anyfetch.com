30a8dcdcef9f14a6cd02929574770baaa5804204
Robin Ricard <ricard.robin@gmail.com>
2014-06-18 21:15:47 +0200

    stable tests

--- helpers/subjob.js
+++ helpers/subjob.js
@@ -1,6 +1,7 @@
 "use strict";
 
 module.exports.create = function(queue, type, data) {
+  console.log('spawn', type, data);
   return queue
           .create(type, data)
           .priority('high')
