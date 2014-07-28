27b91ab4f2b212cf77c8daa5325bec0e82abc0b6
buathi_q <buathi_q@epitech.eu>
2014-07-28 10:47:35 +0200

    Use googleapis 1.x

--- test/retrieve.js
+++ test/retrieve.js
@@ -9,21 +9,20 @@ var retrieve = require('../lib/helpers/retrieve.js');
 describe("Retrieve code", function () {
   it("should list files", function (done) {
     async.waterfall([
-      function getClient(cb) {
-        googleapis.discover('drive', 'v2').execute(cb);
+      function refreshToken(cb) {
+        var oauth2Client = new googleapis.auth.OAuth2(config.googleId, config.googleSecret, config.providerUrl + "/init/callback");
+        oauth2Client.refreshToken_(config.testRefreshToken, rarity.carryAndSlice([oauth2Client], 3, cb));
       },
-      function refreshToken(client, cb) {
-        var oauth2Client = new googleapis.OAuth2Client(config.googleId, config.googleSecret, config.providerUrl + "/init/callback");
-        oauth2Client.refreshToken_(config.testRefreshToken, rarity.carryAndSlice([oauth2Client, client], 4, cb));
-      },
-      function callRetrieve(oauth2Client, client, tokens, cb) {
+      function callRetrieve(oauth2Client, tokens, cb) {
         var options = {
           maxResults: 1000,
           includeDeleted: false
         };
         
         oauth2Client.credentials = tokens;
-        retrieve(client, oauth2Client, options, [], cb);
+        options.auth = oauth2Client;
+
+        retrieve(options, [], cb);
       },
       function checkChanges(newCursor, changes, cb) {
         changes.should.have.lengthOf(4);
@@ -34,21 +33,21 @@ describe("Retrieve code", function () {
 
   it("should list contacts modified after specified date", function (done) {
     async.waterfall([
-      function getClient(cb) {
-        googleapis.discover('drive', 'v2').execute(cb);
+      function refreshToken(cb) {
+        var oauth2Client = new googleapis.auth.OAuth2(config.googleId, config.googleSecret, config.providerUrl + "/init/callback");
+        oauth2Client.refreshToken_(config.testRefreshToken, rarity.carryAndSlice([oauth2Client], 3, cb));
       },
-      function refreshToken(client, cb) {
-        var oauth2Client = new googleapis.OAuth2Client(config.googleId, config.googleSecret, config.providerUrl + "/init/callback");
-        oauth2Client.refreshToken_(config.testRefreshToken, rarity.carryAndSlice([oauth2Client, client], 4, cb));
-      },
-      function callRetrieve(oauth2Client, client, tokens, cb) {
+      function callRetrieve(oauth2Client, tokens, cb) {
         var options = {
           maxResults: 1000,
           includeDeleted: true,
           startChangeId: '66'
         };
+
         oauth2Client.credentials = tokens;
-        retrieve(client, oauth2Client, options, [], cb);
+        options.auth = oauth2Client;
+
+        retrieve(options, [], cb);
       },
       function checkChanges(newCursor, changes, cb) {
         changes.should.have.lengthOf(1);
