e33965051282fb174ab3f840a486578d45831efd
buathi_q <buathi_q@epitech.eu>
2014-11-20 15:27:06 +0100

    Update tests

--- test/upload.js
+++ test/upload.js
@@ -9,7 +9,7 @@ require('should');
 var config = require('../config/configuration.js');
 var serverConfig = require('../lib/');
 
-describe("Workflow", function () {
+describe("Workflow", function() {
   before(AnyFetchProvider.debug.cleanTokens);
 
   // Create a fake HTTP server
@@ -32,24 +32,8 @@ describe("Workflow", function () {
   });
 
   it("should upload data to AnyFetch", function(done) {
-    var originalQueueWorker = serverConfig.workers.addition;
-    var spySendDocumentAndFile;
-
-    serverConfig.workers.addition = function(job, cb) {
-      if(!job.anyfetchClient.sendDocumentAndFile.id) {
-        spySendDocumentAndFile = sinon.spy(job.anyfetchClient, "sendDocumentAndFile");
-      }
-
-      job.task.should.have.property('identifier');
-      job.task.should.have.property('title');
-      job.task.should.have.property('downloadUrl');
-      job.task.should.have.property('type');
-      job.task.should.have.property('createdDate');
-      job.task.should.have.property('modifiedDate');
-
-      originalQueueWorker(job, cb);
-    };
-    var server = AnyFetchProvider.createServer(serverConfig.connectFunctions, serverConfig.updateAccount, serverConfig.workers, serverConfig.config);
+    serverConfig.config.retry = 0;
+    var server = AnyFetchProvider.createServer(serverConfig.connectFunctions, __dirname + '/workers-test.js', __dirname + '/../lib/update.js', serverConfig.config);
 
     request(server)
       .post('/update')
@@ -65,8 +49,15 @@ describe("Workflow", function () {
         }
       });
 
+    server.usersQueue.on('job.task.failed', function(job, err) {
+      done(err);
+    });
+
+    server.usersQueue.on('job.update.failed', function(job, err) {
+      done(err);
+    });
+
     server.usersQueue.once('empty', function() {
-      spySendDocumentAndFile.callCount.should.not.eql(0);
       done();
     });
   });
