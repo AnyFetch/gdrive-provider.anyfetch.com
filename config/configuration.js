1c9b3e71698697004b5aed2517855df8844c072b
buathi_q <buathi_q@epitech.eu>
2014-11-28 11:27:36 +0100

    Decrease timeout of retry

--- config/configuration.js
+++ config/configuration.js
@@ -44,7 +44,7 @@ module.exports = {
   testRefreshToken: process.env.GDRIVE_TEST_REFRESH_TOKEN,
 
   retry: 2,
-  retryDelay: 20 * 1000,
+  retryDelay: 4 * 1000,
 
   opbeat: {
     organizationId: process.env.OPBEAT_ORGANIZATION_ID,
